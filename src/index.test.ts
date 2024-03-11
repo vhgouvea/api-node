9e953b36436991ae245c36762284682f5c389cb6
victorhgouvea <victor.gouveia@allrisk.com.br>
2024-03-11 13:29:11 -0300

    correções no arquivo test.yml

---
+++ src/index.test.ts
@@ -0,0 +1,6 @@
+// describe('greet function', () => {
+//   it('should return a greeting message with the provided name', () => {
+//     const result = greet('World');
+//     expect(result).toBe('Hello, World!');
+//   });
+// });
