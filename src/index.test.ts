a7f16538c68339667be7efecba8e8c14192920c2
Victor Hugo <vhgouvea@hotmail.com>
2024-03-11 21:30:00 -0300

    adicionado testes com jest e alterações na configuração do deploy

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,14 @@
-// describe('greet function', () => {
-//   it('should return a greeting message with the provided name', () => {
-//     const result = greet('World');
-//     expect(result).toBe('Hello, World!');
-//   });
-// });
+import { add, concatenateStrings, isEven } from ".";
+
+test('soma dois números corretamente', () => {
+  expect(add(3, 5)).toBe(8);
+});
+
+test('concatena duas strings corretamente', () => {
+  expect(concatenateStrings('Hello', ' TypeScript')).toBe('Hello TypeScript');
+});
+
+test('verifica se um número é par corretamente', () => {
+  expect(isEven(10)).toBe(true);
+  expect(isEven(7)).toBe(false);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-// src/index.ts
-const greet = (name: string): string => {
-  return `Hello, ${name}!`;
-};
 
-console.log(greet('World'));
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+
+export function concatenateStrings(str1: string, str2: string): string {
+  return str1 + str2;
+}
+
+export function isEven(num: number): boolean {
+  return num % 2 === 0;
+}
